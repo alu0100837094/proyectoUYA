4bcf9343adaf705b48d2f363f202712a338f322f
José Joaquín Escobar Gómez <quinoescobar@gmail.com>
2015-06-03 10:29:40 +0100

    no funciona

--- login.php
+++ login.php
@@ -29,11 +29,11 @@ $password = stripslashes($password);
 $useremail = mysql_real_escape_string($useremail);
 $password = mysql_real_escape_string($password);
 // Selecting Database
-//$db = mysql_select_db($dbname, $connection);
+$db = mysql_select_db($dbname, $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from MyGuests where password='$password' AND email=''$useremail''", $connection);
+$query = mysql_query("select * from MyGuests where password='$password' AND email='$useremail'", $connection);
 $rows = mysql_num_rows($query);
-if ($rows == 0) {
+if ($rows == 1) {
 $_SESSION['login_user']=$useremail; // Initializing Session
 header("location: perfil.php"); // Redirecting To Other Page
 } else {
