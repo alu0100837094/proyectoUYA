1d843295d29656876b346778974573c1d26dceef
José Joaquín Escobar Gómez <quinoescobar@gmail.com>
2015-06-04 12:45:05 +0100

    modificado el login.php

--- login.php
+++ login.php
@@ -11,8 +11,9 @@ $password2 = "5sw5CK";
 $dbname = "alu4635";
 
 // Estableciendo la conexión con el servidor, pasandole como parametros el servername, el user_id y la password y el nombre de la basedatos
-$connection = new mysqli($servername, $username2, $password2, $dbname);
-
+//$connection = new mysqli($servername, $username2, $password2, $dbname);
+$connection = mysql_connect($servername, $username2, $password2) or die("Could not connect database");
+mysql_select_db($dbname, $connection) or die("Could not select database");
 // Para proteger de inyección MySQL, para propositos de seguridad
 $useremail = stripslashes($useremail);
 $password = stripslashes($password);
@@ -22,38 +23,38 @@ $password = mysql_real_escape_string($password);
 $db = mysql_select_db($dbname, $connection);
 // SQL query to fetch information of registerd users and finds user match.
 
-//$query = mysql_query("select * from MyGuests where password='".$password."' AND email='".$useremail."'", $connection);
-$qry = "SELECT *  FROM users WHERE email='".useremail."' AND password='".$password."' ";
+$query = mysql_query("select * from MyGuests where password='".$password."' AND email='".$useremail."'", $connection);
+//$qry = "SELECT *  FROM users WHERE email='".useremail."' AND password='".$password."' ";
 
-if ($result = mysqli_query($connection, $qry)) {
-   // printf("Select returned %d rows.\n", mysqli_num_rows($result));
+// if ($result = mysqli_query($connection, $qry)) {
+//    // printf("Select returned %d rows.\n", mysqli_num_rows($result));
+//
+//     /* free result set */
+//
+// }
+//
+// if($result === false) {
+//     var_dump(mysqli_error($connection));
+// }else{
+//
+//
+// //$res = mysqli_query($con,$qry);
+// $num_row = mysqli_num_rows($result);
+// $row=mysqli_fetch_assoc($result);
+// if( $num_row == 1 ) {
+// 	echo 'true';
+// 	$_SESSION['uName'] = $row['username'];
+// 	$_SESSION['Id'] = $row['id'];
+//
+// 	}
+// else {
+// 	echo 'false';
+// }
+// }
 
-    /* free result set */
-
-}
-
-if($result === false) {
-    var_dump(mysqli_error($connection));
-}else{
 
 
-//$res = mysqli_query($con,$qry);
-$num_row = mysqli_num_rows($result);
-$row=mysqli_fetch_assoc($result);
-if( $num_row == 1 ) {
-	echo 'true';
-	$_SESSION['uName'] = $row['username'];
-	$_SESSION['Id'] = $row['id'];
-
-	}
-else {
-	echo 'false';
-}
-}
-
-
-
-$rows = mysql_num_rows($qry);
+$rows = mysql_num_rows($query);
 if ($rows == 1) {
 $_SESSION['login_user']=$useremail; // Initializing Session
 header("location: perfil.php"); // Redirecting To Other Page
--- script.js
+++ script.js
@@ -4,25 +4,29 @@ $(document).ready(function(){
 	 $("#login").click(function(){
 		  username=$("#email").val();
 		  password=$("#password").val();
+			alert(username);
+			alert(password);
 		  $.ajax({
 		   type: "POST",
 		   url: "login.php",
 			 data: "email="+username+"&password="+password,
-		   success: function(html){
+			 success: function(html){
 			if(html=='true')    {
 			 //$("#add_err").html("right username or password");
+			 //Aquí pasa algo
+			alert("dentro del true");
 			 window.location="perfil.php";
 			}
 			else    {
-			$("#add_err").css('display', 'inline', 'important');
+			 $("#add_err").css('display', 'inline', 'important');
 			 $("#add_err").html("<img src='images/alert.png' class='responsive' alt='imagen_alerta' width='100' height='100'/>Email o Contraseña incorrecta");
 			}
 		   },
-		   beforeSend:function()
-		   {
-			$("#add_err").css('display', 'inline', 'important');
-			$("#add_err").html("<img src='images/ajax-loader.gif' /> Loading...")
-		   }
+		  //  beforeSend:function()
+		  //  {
+			// $("#add_err").css('display', 'inline', 'important');
+			// $("#add_err").html("<img src='images/ajax-loader.gif' /> Loading...")
+		  //  }
 		  });
 		return false;
 	});
