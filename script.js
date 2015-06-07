cfa92e7c90c7e1a2484335deb412d586bfe72f66
José Joaquín Escobar Gómez <quinoescobar@gmail.com>
2015-06-07 22:31:28 +0100

    script de diego

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-$(document).ready(function()
-{
+$(document).ready(function(){
 
-	 //$("#add_err").css('display', 'none', 'important');
-	 $("#login").click(function()
-	{
-			debugger;
-		  email=$("#email").val();
-		  password=$("#password").val();
-			alert(email);
-			alert(password);
-		  $.ajax({
-				data:"email="+email+"&password="+password,
-			  url:'login.php',
-			  type:'post',
-				beforeSend:function()
-				{
-					$("#add_err").css('display', 'inline', 'important');
-					$("#add_err").html("<img src='images/ajax-loader.gif' class='responsive' alt='imagen_cargando' width='100' height='100'/> Loading...")
-				},
-				 success:function(response)
-				{
+ $("#add_err").css('display', 'none', 'important');
+  $("#login").click(function(){
+   debugger;
+    email=$("#email").val();
+    password=$("#password").val();
+    $.ajax({
+     type: "POST",
+     url: "login.php",
+   data: "email="+email+"&password="+password,
+     success: function(html){
 
-				window.location='perfil.php';
-				// if(response=='true')
-				// {
-				//  $("#add_err").html("Email y contraseñas OK");
-				//  //Aquí pasa algo
-				// alert("dentro del true");
-				//  window.location="perfil.php";
-				// }
-				// else
-				// {
-				//  $("#add_err").css('display', 'inline', 'important');
-				//  $("#add_err").html("<img src='images/alert.png' class='responsive' alt='imagen_alerta' width='100' height='100'/>Email o Contraseña incorrecta");
-				// }
-			  }
-		  });
-		return false;
-	});
+      alert("Entre al succes"+html);
+     window.location="perfil.php";
+
+
+   /*if(html=='true')    {
+    //$("#add_err").html("right username or password");
+    window.location="perfil.php";
+   }
+   else    {
+   $("#add_err").css('display', 'inline', 'important');
+    $("#add_err").html("<img src='images/alert.png' />Wrong username or password");
+   }*/
+     },
+     error:function(){
+      alert("Error con ajax");
+      },
+     beforeSend:function()
+     {
+   $("#add_err").css('display', 'inline', 'important');
+   $("#add_err").html("<img src='images/ajax-loader.gif' /> Loading...")
+     }
+    });
+  return false;
+ });
 
 
 });
