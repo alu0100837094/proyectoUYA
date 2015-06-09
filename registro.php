37525fed3c18707c1de61590fe75f59b7a30fc8a
José Joaquín Escobar Gómez <quinoescobar@gmail.com>
2015-06-09 18:26:23 +0100

    sin probar

---
+++ registro.php
@@ -0,0 +1,36 @@
+<?PHP
+//Datos que recibe PHP desde el script
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$emailR=$_POST['emailR'];
+$passwordR=$_POST['passwordR'];
+$passwordR2=$_POST['passwordR2'];
+//Datos para conectarse a la base de datos
+$servername = "localhost";
+$username2 = "alu4635";
+$password2 = "5sw5CK";
+$dbname = "alu4635";
+//Conectando a la base de datos
+$connection = mysql_connect($servername, $username2, $password2,$dbname) or die("Could not connect database :" . mysql_error());
+//Seleccionando la base de datos
+$db = mysql_select_db($dbname, $connection) or die("no se pudo seleccionar la base de datos : " . mysql_error());
+//query para comprobar si el email ya esta en la BBDD
+$comprobar = mysql_query("SELECT * FROM MyGuests WHERE email='$emailR'", $connection) or die('Consulta fallida :' .mysql_error());
+$rows = mysql_num_rows($comprobar);
+if ($rows == 1) {
+echo "false"; // Falso, ya hay un usuario con ese email
+} else {
+//echo"Contraseña y/o email incorrectos";
+$ingresar = mysql_query("INSERT INTO MyGuests (firstname,lastname,email,password) VALUES ('$nombre','$apellidos','$emailR','$passwordR')",$connection) or die('Ingreso fallido : ' . mysql_error());
+echo "true";
+//echo "Correo electronico o contraseña invalidos";
+//echo '<script type="text/javascript"> 	$("#add_err").html("<p>Compruebe que el email y la contraseña esten correctos</p>");</script>';
+//$error = "Email or Password Invalida";
+}
+mysql_close($connection); // Cerrando conexión
+}
+
+
+
+
+?>
--- script.js
+++ script.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
   return false;
 }); //fin function de login
 
-
+//______________________________________________________________________________________________________________________
 
  $("registrar").click(function(){
    nombre=$("#nombre").val();
@@ -53,8 +53,9 @@ $(document).ready(function(){
    emailR=$("#emailR").val();
    passwordR=$("#passwordR").val();
    passwordR2=$('#passwordR2').val();
+   alert(nombre+apellidos+emailR+passwordR2+passwordR);
 
-   if(emailR =='' || passwordR =='' || apellidos == '' || nombre == ''){
+   if(emailR =='' || passwordR =='' || apellidos == '' || nombre == '' || passwordR2 == ''){
      $("#add_errR").html("<p>Llene todos los campos</p>");
    }
    else
@@ -62,7 +63,7 @@ $(document).ready(function(){
      $.ajax({
      type: "POST",
      url: "registro.php",
-     data: "nombre="+nombre+"&apellidos="+apellidos+"&emailR="+emailR+"&passwordR="+passwordR,
+     data: "nombre="+nombre+"&apellidos="+apellidos+"&emailR="+emailR+"&passwordR="+passwordR+"&passwordR2="+passwordR2,
      success: function(html){
 
      alert("Entre al succes de registro"+html);
@@ -71,11 +72,11 @@ $(document).ready(function(){
 
   if(html=='true')    {
 
-   window.location="perfil.php";//aquí que direccione a una página que diga Gracias por registrarse
+   window.location="registroExitoso.html";//aquí que direccione a una página que diga Gracias por registrarse
   }
   else    {
   $("#add_err").css('display', 'inline', 'important');
-   $("#add_err").html("<img src='images/alert.png' />Wrong username or password");
+   $("#add_err").html("<img src='images/alert.png' />Correo electrónico ya registrado");
   }
     },
     error:function(){
