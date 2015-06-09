2c8c4cbdbed9e11912d0b67a54aa66fd00bb66a3
José Joaquín Escobar Gómez <quinoescobar@gmail.com>
2015-06-09 17:46:16 +0100

    modificado el inicio.php preparandolo para el registro ....

--- script.js
+++ script.js
@@ -52,6 +52,47 @@ $(document).ready(function(){
    apellidos=$("#apellidos").val();
    emailR=$("#emailR").val();
    passwordR=$("#passwordR").val();
+   passwordR2=$('#passwordR2').val();
+
+   if(emailR =='' || passwordR =='' || apellidos == '' || nombre == ''){
+     $("#add_errR").html("<p>Llene todos los campos</p>");
+   }
+   else
+   {
+     $.ajax({
+     type: "POST",
+     url: "registro.php",
+     data: "nombre="+nombre+"&apellidos="+apellidos+"&emailR="+emailR+"&passwordR="+passwordR,
+     success: function(html){
+
+     alert("Entre al succes de registro"+html);
+    //window.location="perfil.php";
+
+
+  if(html=='true')    {
+
+   window.location="perfil.php";//aquí que direccione a una página que diga Gracias por registrarse
+  }
+  else    {
+  $("#add_err").css('display', 'inline', 'important');
+   $("#add_err").html("<img src='images/alert.png' />Wrong username or password");
+  }
+    },
+    error:function(){
+     alert("Error con ajax");
+     },
+ //    beforeSend:function()
+ //    {
+ //  $("#add_err").css('display', 'inline', 'important');
+ //  $("#add_err").html("<img src='images/ajax-loader.gif' class='responsive' alt='imagen_cargando' width='50' height='50' /> Loading...")
+ //    }
+   });
+   }
+
+
+ return false;
+}); //fin function de registro
+
 
 
  });
